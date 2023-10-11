5604072bdb327fca1df44196e0e055512945b5f1
Mayank Tripathi <84989130+MAYANK-TRIPATH@users.noreply.github.com>
2023-10-11 15:49:46 +0530

    Course page str

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,34 +1,58 @@
 import React from 'react';
 import styles from './page.module.css'
 import Link from "next/link";
+import Image from "next/image"
+import Choose1 from "public/Choose1.jpg"
 
 const Courses = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.mainTitle}>Our Courses</h1>
-      <h1 className={styles.selectTitle}>Choose our courses</h1>
-      <div className={styles.items}>
-        <Link href="/#" className={styles.item}>
-          <span className={styles.title}>Choose 1</span>
-        </Link>
-        <Link href="/#" className={styles.item}>
-          <span className={styles.title}>Choose 2</span>
-        </Link>
-        <Link href="/#" className={styles.item}>
-          <span className={styles.title}>Choose 3</span>
-        </Link>
-      </div>
-      <div className={styles.items2}>
-        <Link href="/#" className={styles.item}>
-          <span className={styles.title}>Choose 1</span>
-        </Link>
-        <Link href="/#" className={styles.item}>
-          <span className={styles.title}>Choose 2</span>
-        </Link>
-        <Link href="/#" className={styles.item}>
-          <span className={styles.title}>Choose 3</span>
-        </Link>
-      </div>
+    <div className={styles.mainContainer}>
+      <Link href="/" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image src={Choose1} alt="" width={400} height={250} className={styles.image}/>
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Course1</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur, adipisicing 
+          elit. Aspernatur recusandae ipsa molestias? Numquam consequuntur, quas aperiam
+           odio maxime praesentium eligendi quod voluptatum accusamus autem? Sit, iure 
+           repellat. Distinctio, laudantium expedita?</p>
+        </div>
+      </Link>
+      <Link href="/" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image src={Choose1} alt="" width={400} height={250} className={styles.image}/>
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Course2</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing 
+          elit. Non nostrum eaque adipisci at magnam veniam necessitatibus repellat aliquid
+           minus debitis quis a id omnis quas iusto magni, consectetur reprehenderit in.</p>
+        </div>
+      </Link>
+      <Link href="/" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image src={Choose1} alt="" width={400} height={250} className={styles.image}/>
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Course3</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex veritatis 
+          autem maiores voluptatum asperiores, commodi voluptate expedita facere tenetur ratione
+           eius ipsam odio? Similique laboriosam nam perspiciatis minima ipsam placeat?</p>
+        </div>
+      </Link>
+      <Link href="/" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image src={Choose1} alt="" width={400} height={250} className={styles.image}/>
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Course4</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus
+           optio expedita impedit repudiandae quae. Sunt impedit fugiat magni aliquam natus
+            aspernatur exercitationem? Vel nesciunt dicta doloribus iusto sequi. Blanditiis,
+             cupiditate?</p>
+        </div>
+      </Link>
     </div>
   )
 }
