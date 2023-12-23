59e3d08ee76f60a907ebf7a8fcbe98e4ea9d5d2b
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2023-12-23 16:01:17 +0530

    Emailjs Integration done

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+"use client"
+
+import React, { useRef } from 'react'
 import styles from './page.module.css'
 import callus from "public/callus.png"
 import Image from "next/image";
-import Button from '@/components/Button/Button';
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
+  const form = useRef();
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_5ykfz0k', 'template_k3v79mn', form.current, 'jCQV83uOwjRpzn8Ll')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return  (
     <div className={styles.container}>
     <h1 className={styles.title}>Lets Keep in Touch</h1>
@@ -12,13 +29,14 @@ const Contact = () => {
       <div className={styles.imgContainer}>
       <Image src={callus} alt="" className={styles.image} />
       </div>
-      <form className={styles.form}>
-        <input type="text" placeholder="Name" className={styles.input}/>
-        <input type="text" placeholder="Email" className={styles.input}/>
-        <input type="text" placeholder="Phone Number" className={styles.input}/>
+      <form ref={form} onSubmit={sendEmail} className={styles.form}>
+        <input type="text" name="user_name" placeholder="Name" className={styles.input}/>
+        <input type="text" name="user_email" placeholder="Email" className={styles.input}/>
+        <input type="text" name="user_phone" placeholder="Phone Number" className={styles.input}/>
         <textarea className={styles.textArea} placeholder="Message" cols="30" rows="10"></textarea>
           <br/>       
-        <Button url="#" text="Send"/>
+          <input type="submit" value="Send"/>
+       
       </form>
       </div> 
     </div>
