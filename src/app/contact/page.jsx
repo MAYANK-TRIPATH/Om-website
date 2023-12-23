180cbf36b421559da040cef196fb9f444cf0a238
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2023-12-23 17:01:42 +0530

    improved template and UI

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,46 +1,68 @@
 "use client"
 
-import React, { useRef } from 'react'
-import styles from './page.module.css'
-import callus from "public/callus.png"
-import Image from "next/image";
+import React, { useRef, useState } from 'react';
+import styles from './page.module.css';
+import callus from 'public/callus.png';
+import Image from 'next/image';
 import emailjs from '@emailjs/browser';
-
 
 const Contact = () => {
   const form = useRef();
-
+  const [showSuccess, setShowSuccess] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_5ykfz0k', 'template_k3v79mn', form.current, 'jCQV83uOwjRpzn8Ll')
-      .then((result) => {
+    emailjs
+      .sendForm('service_5ykfz0k', 'template_k3v79mn', form.current, 'jCQV83uOwjRpzn8Ll')
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+          // Show success message
+          setShowSuccess(true);
+          // Reset the form
+          form.current.reset();
+          // Hide success message after 3000 milliseconds (3 seconds)
+          setTimeout(() => {
+            setShowSuccess(false);
+          }, 3000);
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
-  return  (
+  return (
     <div className={styles.container}>
-    <h1 className={styles.title}>Lets Keep in Touch</h1>
-    <div className={styles.content}>
-      <div className={styles.imgContainer}>
-      <Image src={callus} alt="" className={styles.image} />
+      <h1 className={styles.title}>Lets Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src={callus} alt="" className={styles.image} />
+        </div>
+        <form ref={form} onSubmit={sendEmail} className={styles.form}>
+          <input type="text" name="user_name" placeholder="Name" className={styles.input} />
+          <input type="text" name="user_email" placeholder="Email" className={styles.input} />
+          <input type="text" name="user_phone" placeholder="Phone Number" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            name="message"
+            placeholder="Message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <br />
+          <input type="submit" value="Send" className={styles.button} />
+        </form>
       </div>
-      <form ref={form} onSubmit={sendEmail} className={styles.form}>
-        <input type="text" name="user_name" placeholder="Name" className={styles.input}/>
-        <input type="text" name="user_email" placeholder="Email" className={styles.input}/>
-        <input type="text" name="user_phone" placeholder="Phone Number" className={styles.input}/>
-        <textarea className={styles.textArea} name="message" placeholder="Message" cols="30" rows="10"></textarea>
-          <br/>       
-          <input type="submit" value="Send"/>
-       
-      </form>
-      </div> 
+      {showSuccess && (
+        <div className={styles.successMessage}>
+          <p><b>Your message has been sent successfully!</b></p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
+
