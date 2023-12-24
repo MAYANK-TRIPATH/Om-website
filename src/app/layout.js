c982cef93bfe9cda3c5775dbe6bc7283a9c8cf23
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-12-24 23:54:48 +0530

    about section updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,22 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar/Navbar'
 import Footer from '@/components/footer/Footer'
 import { ThemeProvider } from '../../context/ThemeContext'
+import Head from 'next/head'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Om computer Institute',
-  description: 'Created by Om institute',
+  title: 'Om Computer Institute',
+  description: 'Created by Om Institute',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en"> 
+    <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider>
       <div className="container">
