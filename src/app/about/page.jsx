75f324bf5db35738c0ef2a79dec95e961d47bd0c
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-12-24 22:33:45 +0530

    editing the about info

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -16,24 +16,22 @@ const About = () => {
       alt="websites"
       className={styles.img}
       />
-     <div className={styles.imgText}>
-      <h1 className={styles.imgTitle}>Om Skills Computer Institute</h1>
-      <h2 className={styles.imgDesc}> Empowering Dreams: The Journey of Om Computer Institute in Prayagraj</h2>
+    <div className={styles.imgText}>
+      <h1 className={styles.imgTitle}>Om Computer Institute</h1>
       </div> 
+
+      <h2 className={styles.imgDesc}> Empowering Dreams: The Journey of Om Computer Institute in Prayagraj</h2>
     </div>
     <div className={styles.textContainer}>
       <div className={styles.item}>
         <h1 className={styles.title}>Who Are We?</h1>
         <p className={styles.desc}>
-        In the bustling city of Prayagraj, where academic aspirations thrive, Om Computer Institute stands tall as a beacon of skill development and empowerment. The brainchild of Mr. Ruchin Verma, the institute's inception in 2015 marked a significant stride in addressing the dearth of vocational skills among students from remote areas and villages. Formerly known as <b>Om Typing Centre</b>, the institute recognized the crucial role of typing skills in various competitive examinations, a need often overlooked.
+        In the bustling city of Prayagraj, where academic aspirations thrive, Om Computer Institute stands tall as a beacon of skill development and empowerment. The brainchild of <b>Mr. Ruchin Verma</b>, the institute's inception in <b>2015</b> marked a significant stride in addressing the dearth of vocational skills among students from remote areas and villages. Formerly known as <b>Om Typing Centre</b>, the institute recognized the crucial role of typing skills in various competitive examinations, a need often overlooked. <br/>
 
           <br/>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magni odit nisi sapiente 
-          possimus facere hic alias amet dolorum laudantium accusantium, veritatis illum quos id du
-          cimus soluta temporibus vero deleniti.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magni odit nisi sapiente 
-          possimus facere hic alias amet dolorum laudantium accusantium, veritatis illum quos id du
-          cimus soluta temporibus vero deleniti.
+        <b>A Humble Beginning:</b> <br/>
+The first branch near Salori, IERT College, Prayagraj, was a modest start that burgeoned into a success story. Understanding the challenges faced by students preparing for competitive exams in the hub area, the institute grew steadily, achieving remarkable heights by securing over a thousand selections in prestigious examinations like High Court Review Officer, Assistant Review Officer, Stenographer, and Constable.
+
         </p>
       </div>
       <div className={styles.item}>
