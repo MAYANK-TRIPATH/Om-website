66eba1da241e2ccc9c8406588963a81de61cbb90
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2023-12-29 02:47:32 +0530

    Course resolved

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import styles from './page.module.css'
 import Link from "next/link";
 import Image from "next/image"
-import Course1 from "public/Course1.png"
-import Course2 from "public/Course2.png"
+import course1 from "public/course1.png"
+import course2 from "public/course2.png"
 
 const Courses = () => {
   return (
     <div className={styles.mainContainer}>
-      <h1 className={styles.mainTitle}>Courses Offered</h1>
       <Link href="/" className={styles.container}>
         <div className={styles.imageContainer}>
-          <Image src={Course1} alt="" width={400} height={250} className={styles.image}/>
+          <Image src={course1} alt="" width={400} height={250} className={styles.image} />
         </div>
         <div className={styles.content}>
           <h1 className={styles.title}>'O' Level</h1>
@@ -20,28 +19,27 @@ const Courses = () => {
       </Link>
       <Link href="/" className={styles.container}>
         <div className={styles.imageContainer}>
-          <Image src={Course2} alt="" width={400} height={250} className={styles.image}/>
+          <Image src={course2} alt="" width={400} height={250} className={styles.image} />
         </div>
         <div className={styles.content}>
           <h1 className={styles.title}>ADCA + TALLY</h1>
           <p className={styles.desc}>The Advanced Diploma in Computer Applications (ADCA) is a one-year course that includes computer training and accounting practical knowledge on Tally ERP. The ADCA course prepares students for work in the field of computers and software. It includes computer fundamentals, database management systems, operation systems, web designing, programming with C and C++, graphics, animation, and multimedia. 
-After completing the ADCA course, many small firms recruit candidates for jobs such as computer operator and data entry. 
-</p>
+After completing the ADCA course, many small firms recruit candidates for jobs such as computer operator and data entry.</p>
         </div>
       </Link>
       <Link href="/" className={styles.container}>
         <div className={styles.imageContainer}>
-          <Image src={Course1} alt="" width={400} height={250} className={styles.image}/>
+          <Image src={course1} alt="" width={400} height={250} className={styles.image} />
         </div>
         <div className={styles.content}>
           <h1 className={styles.title}>DCA</h1>
-          <p className={styles.desc}> Diploma in Computer Applications, is a 1-year diploma course in the field of Computer Applications that involves the study of numerous computer applications such as MS Office, Internet Applications, Operating System, Database Management System (DBMS), HTML among other subjects.
+          <p className={styles.desc}>Diploma in Computer Applications, is a 1-year diploma course in the field of Computer Applications that involves the study of numerous computer applications such as MS Office, Internet Applications, Operating System, Database Management System (DBMS), HTML among other subjects.
 <br/> DCA Syllabus includes subjects such as Basics Computer Skills, MS Office Applications, Internet Basics, E-Business, PC Assembly and Troubleshooting.</p>
         </div>
       </Link>
       <Link href="/" className={styles.container}>
         <div className={styles.imageContainer}>
-          <Image src={Course2} alt="" width={400} height={250} className={styles.image}/>
+          <Image src={course2} alt="" width={400} height={250} className={styles.image} />
         </div>
         <div className={styles.content}>
           <h1 className={styles.title}>CCC</h1>
@@ -50,7 +48,7 @@ After completing the ADCA course, many small firms recruit candidates for jobs s
       </Link>
       <Link href="/" className={styles.container}>
         <div className={styles.imageContainer}>
-          <Image src={Course1} alt="" width={400} height={250} className={styles.image}/>
+          <Image src={course1} alt="" width={400} height={250} className={styles.image} />
         </div>
         <div className={styles.content}>
           <h1 className={styles.title}>AUTOCAD(2D/3D)</h1>
@@ -59,15 +57,15 @@ After completing the ADCA course, many small firms recruit candidates for jobs s
       </Link>
       <Link href="/" className={styles.container}>
         <div className={styles.imageContainer}>
-          <Image src={Course2} alt="" width={400} height={250} className={styles.image}/>
+          <Image src={course2} alt="" width={400} height={250} className={styles.image} />
         </div>
         <div className={styles.content}>
           <h1 className={styles.title}>Basic Computer</h1>
-          <p className={styles.desc}> In step with effort to achieve the objective of achieving computer literacy in an inclusive manner, NIELIT has launched a new programme, “Basic Computer Course (BCC)”. The objective of the course is to impart basic level computer appreciation programme with more emphasis on hands on training. Initially, the course was conceived as to boost the concept of introducing a course on computer fundamentals, especially for students, studying vocational courses at ITIs/ITCs.</p>
+          <p className={styles.desc}>In step with effort to achieve the objective of achieving computer literacy in an inclusive manner, NIELIT has launched a new programme, “Basic Computer Course (BCC)”. The objective of the course is to impart basic level computer appreciation programme with more emphasis on hands on training. Initially, the course was conceived as to boost the concept of introducing a course on computer fundamentals, especially for students, studying vocational courses at ITIs/ITCs.</p>
         </div>
       </Link>
     </div>
   )
 }
 
-export default Courses
+export default Courses;
