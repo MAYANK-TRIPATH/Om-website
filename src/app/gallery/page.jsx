429df5a31bcfd8217973b6f945480c2d5f725986
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-12-29 16:50:08 +0530

    Gallery changes

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -9,35 +9,61 @@ const Gallery = () => {
       <h1 className={styles.mainTitle}>Photo Gallery</h1>
       <div className={styles.items}>
         <Link href="" className={styles.item}>
-          <span className={styles.title}>Choose 1</span>
+          {/* <span className={styles.title}>Choose 1</span> */}
         </Link>
         <Link href="" className={styles.item}>
-          <span className={styles.title}>Choose 2</span>
+          {/* <span className={styles.title}>Choose 2</span> */}
         </Link>
         <Link href="" className={styles.item}>
-          <span className={styles.title}>Choose 3</span>
+          {/* <span className={styles.title}>Choose 3</span> */}
         </Link>
       </div>
+
       <div className={styles.items2}>
         <Link href="" className={styles.item1}>
-          <span className={styles.title}>Choose 4</span>
+          {/* <span className={styles.title}>Choose 4</span> */}
         </Link>
         <Link href="" className={styles.item1}>
-          <span className={styles.title}>Choose 5</span>
+          {/* <span className={styles.title}>Choose 5</span> */}
         </Link>
         <Link href="" className={styles.item1}>
-          <span className={styles.title}>Choose 6</span>
+          {/* <span className={styles.title}>Choose 6</span> */}
         </Link>
       </div>
+
       <div className={styles.items2}>
         <Link href="" className={styles.item2}>
-          <span className={styles.title}>Choose 7</span>
+          {/* <span className={styles.title}>Choose 7</span> */}
         </Link>
         <Link href="" className={styles.item2}>
-          <span className={styles.title}>Choose 8</span>
+          {/* <span className={styles.title}>Choose 8</span> */}
         </Link>
         <Link href="" className={styles.item2}>
-          <span className={styles.title}>Choose 9</span>
+          {/* <span className={styles.title}>Choose 9</span> */}
+        </Link>
+      </div>
+
+      <div className={styles.items3}>
+        <Link href="" className={styles.item2}>
+          {/* <span className={styles.title}>Choose 7</span> */}
+        </Link>
+        <Link href="" className={styles.item2}>
+          {/* <span className={styles.title}>Choose 8</span> */}
+        </Link>
+        <Link href="" className={styles.item2}>
+          {/* <span className={styles.title}>Choose 9</span> */}
+        </Link>
+      </div>
+
+      <div className={styles.items4}>
+        <Link href="" className={styles.item2}>
+          {/* <span className={styles.title}>Choose 7</span> */}
+        </Link>
+        <Link href="" className={styles.item2}>
+          {/* <span className={styles.title}>Choose 8</span> */}
+        </Link>
+        <Link href="" className={styles.item2}>
+          {/* <span className={styles.title}>Choose 9</span> */}
         </Link>
       </div>
   
